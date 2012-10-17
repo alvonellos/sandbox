c0ab959612c7e9b71a9c7a522d94212ebb292d44
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2012-10-17 19:12:28 -0400

    Sqlite importing of financial data

---
+++ sqlite/ma/import.py
@@ -0,0 +1,26 @@
+import sqlite3 as lite
+import glob
+import os.path
+
+files = r'F:/Alex Stuff/Dropbox/ascii/'
+listing = glob.glob(files + '*.TXT')
+tables =  [os.path.split(y)[1] for y in [os.path.splitext(x)[0] for x in listing]]
+print tables
+db = './data.db'
+
+
+def main():
+	con = lite.connect(db)
+	cur = con.cursor()
+	
+	cur.execute('DROP TABLE IF EXISTS TICKERS')
+	cur.execute('CREATE TABLE TICKERS (id INTEGER PRIMARY KEY AUTOINCREMENT, SYMBOL TEXT)')
+	cur.executemany('INSERT INTO TICKERS VALUES(NULL, ?)', zip(tables))
+
+#	cur.executemany('CREATE TABLE ? (id integer)', zip(tables))
+	con.commit()
+	con.close()
+
+
+if __name__ == "__main__":
+	main()
